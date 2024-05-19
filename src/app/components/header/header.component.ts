f4b4f45ee1bacca86ef20126b16182b562d1d0a5
Rolo27s <franrodriguez2727@gmail.com>
2024-05-19 21:00:02 +0200

    diversificando css y js

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  menuVisible = false;
 
+  toggleMenu(): void {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  closeMenu(): void {
+    this.menuVisible = false;
+  }
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,7 +8,53 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent {
 
+export class MainComponent implements OnInit {
+  currentIndex = 0;
+
+  ngOnInit() {
+    this.initializeCarousel();
+  }
+
+  initializeCarousel() {
+    const carousel = document.querySelector('.carousel') as HTMLElement;
+    const leftArrow = document.querySelector('.left-arrow') as HTMLElement;
+    const rightArrow = document.querySelector('.right-arrow') as HTMLElement;
+    const dots = document.querySelectorAll('.dot');
+
+    const updateCarousel = () => {
+      carousel.style.transform = `translateX(${-this.currentIndex * 25}%)`; // 100% / 4 slides = 25%
+
+      dots.forEach((dot, index) => {
+        dot.classList.toggle('active', index === this.currentIndex);
+        (dot as HTMLElement).style.backgroundColor = index === this.currentIndex ? '#FAB41E' : '#bbb';
+      });
+    };
+
+    const showNext = () => {
+      this.currentIndex = (this.currentIndex + 1) % dots.length;
+      updateCarousel();
+    };
+
+    const showPrevious = () => {
+      this.currentIndex = (this.currentIndex - 1 + dots.length) % dots.length;
+      updateCarousel();
+    };
+
+    const autoSlide = () => {
+      showNext();
+    };
+
+    leftArrow.addEventListener('click', showPrevious);
+    rightArrow.addEventListener('click', showNext);
+
+    dots.forEach((dot, index) => {
+      dot.addEventListener('click', () => {
+        this.currentIndex = index;
+        updateCarousel();
+      });
+    });
+
+    setInterval(autoSlide, 5000);
+  }
 }
-
